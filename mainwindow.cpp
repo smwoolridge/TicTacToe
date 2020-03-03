7002f25255621c8ea873f0d3090b6372ea6e8508
Boonthai Yan <56100316+Thai-bit@users.noreply.github.com>
2020-03-03 11:11:55 -0800

    First commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,79 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+bool MainWindow::checkForWinner(int buttonClicked[], int player)
+{
+    bool winning = true;
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; i < 3; j++){
+            if(buttonClicked[winCombinations[i][j]] != player){
+                winning = false;
+                break;
+            }
+        }
+        if(winning){
+            // set winning player name
+            return true;
+        }
+        winning = true;
+    }
+    return false;
+}
+
+int MainWindow::playerTurn(int index)
+{
+    // check array index
+    if(buttonClicked[index] == 0){
+        clickCount++;
+        checkPlayer = clickCount % 2;
+        switch(checkPlayer){
+        case 1:
+            // switch label to player 2
+            ui->playerLabel->setText("player 2");
+            //change current index 1
+            buttonClicked[index] = 1;
+            return 1;
+        case 0:
+            // switch label to player 1
+            ui->playerLabel->setText("player 1");
+            // change current index 2
+            buttonClicked[index] = 2;
+            return 2;
+        }
+    }
+    return 0;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    int player = playerTurn(0);
+    if(player == 1){
+        ui->pushButton->setText("X");
+       } else if (player == 2){
+        ui->pushButton->setText("O");
+    }
+    checkForWinner(buttonClicked, player);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    int player = playerTurn(1);
+    if(player == 1){
+        ui->pushButton_2->setText("X");
+       } else if (player == 2){
+        ui->pushButton_2->setText("O");
+    }
+    checkForWinner(buttonClicked, player);
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    int player = playerTurn(2);
+    if(player == 1){
+        ui->pushButton_4->setText("X");
+       } else if (player == 2){
+        ui->pushButton_4->setText("O");
+    }
+    checkForWinner(buttonClicked, player);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,33 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+     bool checkForWinner(int buttonClicked[], int player);
+
+private slots:
+     void on_pushButton_clicked();
+
+     void on_pushButton_2_clicked();
+
+     void on_pushButton_4_clicked();
+
 private:
     Ui::MainWindow *ui;
+    int clickCount = 0;
+    int buttonClicked [9] = {0,0,0,0,0,0,0,0,0};
+    int checkPlayer;
+    int playerTurn (int index);
+
+    int winCombinations[8][3] = {{0,3,6}, {0,1,2}, {2,5,8}, {6,7,8}, {0,4,8},{2,4,6},{3,4,5}, {1,4,7}};
+
 };
+
+
+
+
+
+
+
+
 
 #endif // MAINWINDOW_H
